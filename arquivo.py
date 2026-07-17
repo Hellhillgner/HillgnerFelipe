dc351e68168e8c16c71a8bc115abe1b07e7bc040
Hillgner Morel <hillgnermorel@gmail.com>
2026-07-17 13:56:40 -0400

    first commit

---
+++ arquivo.py
@@ -0,0 +1,18 @@
+# Versos íntimos [Augusto dos Anjos]
+# Vês! Ninguém assistiu ao formidável
+# Enterro de sua última quimera.
+# Somente a Ingratidão – esta pantera –
+# Foi tua companheira inseparável!
+
+# Acostuma-te à lama que te espera!
+# O homem, que, nesta terra miserável,
+# Mora, entre feras, sente inevitável
+# Necessidade de também ser fera.
+
+# Toma um fósforo. Acende teu cigarro!
+# O beijo, amigo, é a véspera do escarro,
+# A mão que afaga é a mesma que apedreja.
+
+# Se alguém causa inda pena a tua chaga,
+# Apedreja essa mão vil que te afaga,
+# Escarra nessa boca que te beija!
