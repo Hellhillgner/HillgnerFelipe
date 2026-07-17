db645004bed5219e991a7b80ef7e5bb53995f232
Hillgner Morel <hillgnermorel@gmail.com>
2026-07-17 16:51:42 -0400

    area e inteiros

--- arquivo.py
+++ arquivo.py
@@ -1,40 +1,13 @@
-# from reverter_inteiro import reverter_inteiro
-# from area import calcular_area_trianqulo  
-# print("Olá! Tudo bem coleguinha?")
-# while True:
-#     opcao = input("Se você deseja ver a função do Hillgner digite 1, se deseja ver a função do Felipe digite 2, digite 3 para encerrar o programa\n")
-#     match opcao:
-#         case "1":
-#             continue #atualizar com a função do Hillgner
-#             calcular_area_triangulo()
-#         case "2":
-#             reverter_inteiro()
-#         case "3":
-#             break
-# print("Obrigado por usar esse programa maneiro, até a próxima!")
 from reverter_inteiro import reverter_inteiro
-from area import calcular_area_triangulo
-
+from area import calcular_area_triangulo  
 print("Olá! Tudo bem coleguinha?")
-
 while True:
-    opcao = input(
-        "Digite 1 para ver a função do Hillgner,\n"
-        "Digite 2 para ver a função do Felipe,\n"
-        "Digite 3 para encerrar o programa:\n"
-    )
-
+    opcao = input("Se você deseja ver a função do Hillgner digite 1, se deseja ver a função do Felipe digite 2, digite 3 para encerrar o programa\n")
     match opcao:
         case "1":
             calcular_area_triangulo()
-
         case "2":
             reverter_inteiro()
-
         case "3":
             break
-
-        case _:
-            print("Opção inválida!")
-
 print("Obrigado por usar esse programa maneiro, até a próxima!")
